24acfa181fa7d21d4943baa45bcd0a31b7f142cc
roshani <roshanisalekar3046@gmail.com>
2025-01-09 18:58:23 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,40 @@
+                     #  Python  Datatype # 
+# python int                      
+a=10
+print(a)
+print(type(a))
+
+# python float   
+a=10.98
+print(a)
+print(type(a))
+
+# python string
+a="rosani"
+print(a)
+print(type(a))
+
+# python bool  
+a=True
+print(a)
+print(type(a))
+
+# python tuple
+a=("rosani",9850,4+4j,"raj1",34.67)
+print(a)
+print(type(a))
+
+# python list
+a=[34,"rosani",34.89]
+print(a)
+print(type(a))
+
+# python set
+a={23,"niku",34.90}
+print(a)
+print(type(a))
+
+# python  dictionary
+a={'age':34,'name':"rosani",'age':45}
+print(a)
+print(type(a))
