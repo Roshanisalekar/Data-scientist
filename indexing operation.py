b483010cfe5ec2cb27a5f1dcfb5704940cbef3f5
roshani <roshanisalekar3046@gmail.com>
2025-01-13 21:49:16 +0530

    Add files via upload

---
+++ indexing operation.py
@@ -0,0 +1,31 @@
+#string indexing
+# you can find Manik
+a="My Name is khan"
+b=a[0]+a[4]+a[-1]+a[-7]+a[-4]
+print(b)
+
+
+# you can find Namika
+a="My Name is khan"
+b=a[3]+a[-2]+a[5]+a[8]+a[11]+a[4]
+print(b)
+
+# you can find Manish
+a="My Name is khan"
+b=a[0]+a[4]+a[14]+a[8]+a[9]+a[12]
+print(b)
+
+#tuple indexing
+
+a=(1,'Raj',2,'mayur',3,"rawal")
+b=(a[0],a[2],a[1],a[3])
+print(b)
+# you are find yuwaraj
+c=a[3][2]+a[3][3]+a[5][2]+a[5][3]+a[1]
+print(c)
+
+#list indexing
+a=[1,'raj','mayur','manav',2]
+#you find ravan
+b=[a[0],a[1][0]+a[1][1]+a[3][4]+a[3][1]+a[3][2],a[4]]
+print(b)
---
+++ slice indexing operation.py
@@ -0,0 +1,26 @@
+#string slicing         
+a="My name is Rajendra and he loves python"
+#=012345678910
+#you find Rajendra and he loves
+b=a[11:-7:1]
+print(b)
+
+#you find Rajendra and he loves
+b=a[11:-7:2]  # step :2
+print(b)
+
+#you find Rajendra loves python
+b=a[11:20:1]+a[-12: :1]
+print(b)
+
+#tuple slicing
+a=('a',1,2,'b','raj','abc@1','mahi')
+#you find 2,b,mahi,abc1
+b=(a[2],a[3],a[6],a[5][0:3:1]+a[5][4])
+print(b)
+
+#list slicing
+a=('o',1,9,'b','car','payal','mahesh')
+#you find 
+b=(a[2],a[3],a[6],a[5][0:3:1]+a[5][4])
+print(b)
