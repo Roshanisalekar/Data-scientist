67130b5c56ce5ee763a47eb78c5df6b993bc3aad
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-20 06:39:11 -0700

    Add files via upload

---
+++ continue statement.py
@@ -0,0 +1,11 @@
+# With the continue statement we can stop the current iteration of the loop, and continue with the next:
+
+familymember=["roshani","ankush","sagar","amol",'savita']
+
+for i in familymember:
+    if i=="amol":
+        continue
+    print(i)
+
+
+
