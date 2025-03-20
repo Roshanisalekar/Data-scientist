39cf7bd13b4347874f4851443ed790b4efe813e1
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-19 18:51:02 -0700

    Add files via upload

---
+++ Algorithm sequence.py
@@ -0,0 +1,20 @@
+# sequence algorithm :-
+
+# 1> sequence algorithm are followed top to bottom 
+
+# 2> sequence algorithm is line by line executed
+
+# example :
+
+# area of the circle 
+
+pi=3.14
+r=float(input("enter a radius :-"))
+
+area=pi*r*r
+
+print(f'Area of the circle is :{area}')
+
+output :-
+# enter a radius :-12
+# Area of the circle is :452.15999999999997
