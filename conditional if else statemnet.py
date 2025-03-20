7c22b3c3d49df20be96f52d3df6536ea0bc9df45
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-19 20:18:47 -0700

    Add files via upload

---
+++ conditional if else statemnet.py
@@ -0,0 +1,24 @@
+# if-else statement : the condition is true then block of executed but condition is false then else part will be excuted.
+
+# syntax :
+# if condition:
+#     statement
+# else:
+#     statement
+
+# example :
+
+"check whether age is grater than equal to 18 or not"
+
+age=int(input("enter your age :"))
+if age>=18:
+    print("you can vote!")
+else:
+    print("you cannot vote ")
+print("thanks")
+
+# output:
+
+# enter your age :16
+# you cannot vote 
+# thanks
