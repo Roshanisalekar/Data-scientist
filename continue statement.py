3fff3f09620d310cc3b3a5fe677e80ee86b4dc63
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-20 06:40:07 -0700

    Update continue statement.py

--- continue statement.py
+++ continue statement.py
@@ -1,4 +1,4 @@
-# With the continue statement we can stop the current iteration of the loop, and continue with the next:
+#  continue statement : we can stop the current iteration of the loop, and continue with the next:
 
 familymember=["roshani","ankush","sagar","amol",'savita']
 
