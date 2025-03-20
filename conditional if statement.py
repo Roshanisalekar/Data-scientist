b3404b19e245280abe1874b7b96e7c1255cd707f
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-19 19:42:33 -0700

    Add files via upload

---
+++ conditional if statement.py
@@ -0,0 +1,25 @@
+# if statement : the condition is true then block of executed .
+
+# syntax :
+# if condition:
+#     statement
+
+# example :
+
+"check whether age is grater than equal to 18"
+
+age=int(input("enter your age :"))
+if age>=18:
+    print("you can vote!")
+print("thanks")
+
+# output : 1 
+
+# enter your age :12
+# thanks
+
+# output :  2 
+
+# enter your age :19
+# you can vote!
+# thanks
