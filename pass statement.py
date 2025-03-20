18b52ac98a2e2bfa04e55566d0033fcd9a21ba74
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-20 07:15:21 -0700

    Add files via upload

---
+++ pass statement.py
@@ -0,0 +1,13 @@
+# pass ststement : pass statement are used to execution in  easily continue with the help of pass 
+
+num=[1,2,3,4,5,6,7,8]
+
+for i in num:
+    if i==6:
+        pass
+print(num)
+
+
+# output :
+
+# [1, 2, 3, 4, 5, 6, 7, 8]
