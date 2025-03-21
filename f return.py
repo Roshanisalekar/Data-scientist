ec4d36abef460f79193dbbc20bd0f8637107b1e6
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-21 09:47:58 -0700

    Add files via upload

---
+++ f return.py
@@ -0,0 +1,15 @@
+def full_name(frist_name,last_name):
+    full_name=f'my name is {frist_name} {last_name}'
+    return full_name
+    #print(full_name)
+
+name=full_name("savita","salekar")
+
+print(name)
+
+
+'''
+function naming meaningful
+length of function : limited 
+avoid global variable
+''' 
