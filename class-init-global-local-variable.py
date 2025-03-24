67cb2bdbfa31673da63f60579b37cf29d670b1c3
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-23 21:08:31 -0700

    Update class-init-global-local-variable.py

--- class-init-global-local-variable.py
+++ class-init-global-local-variable.py
@@ -91,3 +91,119 @@ print(mu.m)
 # pawder
 
 #--------------------------------------------------------------------------------------------------------------
+
+#example : 5
+class masale:
+    masala='maggic'
+    def _init_(self,masala1,masala2,masala3):
+
+        self.masala1=masala1
+        self.masala2=masala2
+        self.masala3=masala3
+        
+        
+
+        print(f'{self.masala1} and {self.masala2} and {self.masala3} and {self.masala}')
+
+m=masale("chicken","paneer","misal")
+print(m.masala)
+
+# output:
+# chicken and paneer and misal and maggic
+# maggic
+
+#----------------------------------------------------------------------------------------
+# example :6
+
+class color:
+    c='black'
+    def _init_(self,c1,c2,c3):
+        self.c1=c1
+        self.c2=c2
+        self.c3=c3
+        print(f'{self.c1} vs {self.c2} vs {self.c3} vs {self.c}')
+        
+colours=color('red','blue','green')
+
+col=color('pink','white','orange')
+
+# output:
+# red vs blue vs green vs black
+# pink vs white vs orange vs black
+#----------------------------------------------------------------------------------------
+
+#example : 7
+
+
+class datatype:
+    c='imutable'
+    def _init_(self,d1,d2,d3):
+        self.d1=d1
+        self.d2=d2
+        self.d3=d3
+        print(f'{self.d1} vs {self.d2} vs {self.d3} vs {self.c}')
+        
+types=datatype('complex','float','int')
+
+t1=datatype('tuple','list','string')
+
+# output:
+# complex vs float vs int vs imutable
+# tuple vs list vs string vs imutable
+
+#------------------------------------------------------------------------------------------
+
+# example : 8
+
+class data_sciencetist:
+    subject="data anaytics"
+    def _init_(self,s1,s2,s3,s4,s5):
+        self.s1=s1
+        self.s2=s2
+        self.s3=s3
+        self.s4=s4
+        self.s5=s5
+        print(f"{self.s1} ,{self.s2},{self.s3},{self.s4},{self.s5},{self.subject}")
+
+data=data_sciencetist('python','sql','AL','DL','ML')
+        
+# OUTPUT:
+
+# python ,sql,AL,DL,ML,data anaytics
+
+#--------------------------------------------------------------------------------------
+
+# example :9
+
+class developer:
+    def _init_(self,developer1,developer2,developer3,developer4,developer5):
+        self.developer1=developer1
+        self.developer2=developer2
+        self.developer3=developer3
+        self.developer4=developer4
+        self.developer5=developer5
+        print(f"{developer1},{developer2}")
+        
+        
+dev=developer("java","python","c++","c","c#")
+
+# output:
+    
+# java,python
+    
+#--------------------------------------------------------------------------------------
+        
+# example:10
+
+class car:
+    color='white'
+    style='royal'
+    def _init_(self,brand,color):
+        self.brand=brand
+        self.color=color
+        print(f"{self.brand} is  color {self.color} and {self.style}")
+car1=car('bmw','black')
+
+
+# output:        
+# bmw is color black and royal
