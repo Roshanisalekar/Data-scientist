d1a757f45ba4383cd1e4e7feae66adf7c299d94e
Roshani salekar <roshanisalekar3046@gmail.com>
2025-03-24 22:45:23 -0700

    Update class-multiple-inheritance.py

--- class-multiple-inheritance.py
+++ class-multiple-inheritance.py
@@ -1,3 +1,33 @@
+#----------------------------------------------------------------------------------------------------------------
+# example :- 1 
+
+class A:
+    def a(self):
+        print("class A")
+
+class B(A):
+    def b(self):
+        print("class B")
+
+class c(B):
+    def c(self):
+        print("class c")
+
+c=c()
+
+c.b()
+c.a()
+c.c()
+
+# output :-
+
+# class B
+# class A
+# class c
+
+#----------------------------------------------------------------------------------------------------------------
+# example :- 2
+
 class car():
     def start(self):
         print("car started")
@@ -20,4 +50,101 @@ class fortuner(toyotacar):
 c1=fortuner('red')
 c1.start()
 c1.stop()
-c1.brand('bmw')
+
+
+# output :-
+
+# red
+# car started
+# car stopped
+
+#----------------------------------------------------------------------------------------------------------------
+# example :- 3
+
+class college:
+    def __init__(self,name):
+        self.name=name
+
+class principle(college):
+    def __init__(self,name):
+        self.name=name
+
+    def sayhello(self):
+        print("hello everybudy")
+
+class university(principle):
+    def __init__(self,name):
+        self.name=name
+        print(f"{name}")
+
+u=university('mumbai')
+u.sayhello()
+
+# output:-
+# mumbai
+# hello everybudy
+
+# ----------------------------------------------------------------------------------------------------------------
+# example :- 4
+
+class grandfather:
+    grandfather="chhaberav"
+
+    def brave(self):
+        print("grandfather is brave")
+
+class grandmother(grandfather):
+
+    def strong(self):
+        print("my family ")
+
+
+class family(grandmother):
+    dad="ankush"
+
+    def __init__(self,brother,brother1):
+        self.brother=brother
+        self.brother1=brother1
+        print(f"{brother1}")
+
+f=family("amol",'sagar')
+
+print(f.dad)
+f.strong()
+f.brave()
+
+# output:-
+# sagar
+# ankush
+# my family
+# grandfather is brave
+
+# ----------------------------------------------------------------------------------------------------------------
+# example :- 5
+
+class data_science:
+    trainer="rajendra sir"
+
+    def teach(self):
+        print("teaching is best")
+
+class module(data_science):
+
+    def __init__(self,m1):
+        self.m1=m1
+        print(f"{m1}")
+
+class me(module):
+
+    def me(self):
+        print("i am best student")
+
+
+m=me("python")
+print(m.trainer)
+
+# output:
+# python
+# rajendra sir
+
+#-------------------------------------------------------------------------------------------------------------------
