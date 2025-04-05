32511cbc5b8b9b3d78c812bc14c474421517cd36
Roshani salekar <roshanisalekar3046@gmail.com>
2025-04-05 08:47:41 -0700

    Add files via upload

---
+++ class-inheritance.py
@@ -0,0 +1,20 @@
+# what is inheritance ?
+
+# when we class define a class that inheritance all properties of other class called inheritance
+
+class father:
+    def lands(self):
+        print("50 eker ")
+
+class son(father):
+    def money(self):
+        print("40 lacs")
+
+s=son()
+s.money()
+s.lands()
+
+# output :- 
+
+# 40 lacs
+# 50 eker
