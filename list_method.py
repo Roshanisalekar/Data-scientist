4b1e2d22fe33e9a4a19e7be6882dc7275647e7c1
Roshani salekar <roshanisalekar3046@gmail.com>
2025-04-06 07:34:36 -0700

    Add files via upload

--- list_method.py
+++ list_method.py
@@ -1,208 +1,126 @@
-#-------------------------------------------------------------------------------------------------------------------
-
-                                        # list method 
-
-# 1> append
-# 2> remove 
-# 3> insert 
-# 4> pop 
-# 5> extend 
-# 6> index 
-# 7> copy 
-# 8> reverse 
-# 9> sort
-# 10> sort(reverse) 
-# 11> count 
-# 12> clear
-#---------------------------------------------------------------------------------------------------------------------
-
-# list method 
-a=[1,2,3,"raj",6]
+a=[1,2,3,4,5,6]
+a.append(7)
 print(a)
 
-#------------------------------------------------------------------------------------------------------------------------
-# add element in list
-# 4th  value add in list
-a.append(4)
+a.append(10)
 print(a)
 
-# 8 value add in list
-a.append(8)
+a.append(15)
 print(a)
 
-# 12 value add in list 
-a.append(12)
-print(a) 
-
-# 16 value add in list 
-a.append(16)
+a.extend([8,9,10])
 print(a)
 
-# 20 value add in list 
-a.append(20)
+a.extend([12,34,56])
 print(a)
 
-#-------------------------------------------------------------------------------------------------------------------
-
-# 20 value remove in list
-a.remove(20)
+a.extend(['@',"#","$"])
 print(a)
 
-# 16 value remove in list 
-a.remove(16)
+a.insert(2,'three')
 print(a)
 
-# 12 value remove in list 
-a.remove(12)
+a.insert(0,"roshani")
 print(a)
 
-# 8 value remove in list 
-a.remove(8)
+a.insert(1,"savi")
 print(a)
 
-# 4 value remove in list 
-a.remove(4)
+b=[2,6,9,7,5,3,2]
+b.sort()
+print(b)
+
+b.count(3)
+print(b)
+
+b.sort(reverse=True)
+print(b)
+
+b.reverse()
+print(b)
+
+a.remove('three')
 print(a)
 
-#------------------------------------------------------------------------------------------------------------
+a.pop(9)
+print(a)
+c=a.copy()
+print(c)
 
-# add element in list with the help of list 
+c.clear()
+print(c)
 
-# 4 value add in list
-a.insert(3,4)
+c.clear()
+print(c)
+
+c.append(567)
+print(c)
+
+c.extend([123,456,234,980,6789])
+print(c)
+
+d=c.copy()
+print(d)
+
+a={'dad':50,'mom':48,'sis':20,'bro':18}
+
 print(a)
 
-# 5 value add in list
-a.insert(2,5)
+print(a.keys())
+
+#.values()
+print(a.values())
+
+print(a.items())  #key and value pair with tuple
+
+print(a.get('mom'))
+
+print(a.get("uncle","dont have")) 
 print(a)
 
-# 23 value add in list
-a.insert(1,23)
+print(a.update({'uncle':53}))
 print(a)
 
-# 44 value add in list
-a.insert(0,44)
+print(a.update({'mom':45}))
 print(a)
 
-# 4 value add in list
-a.insert(4,4)
+a.setdefault("mama",45)
 print(a)
 
-#--------------------------------------------------------------------------------------------------------------
-
-# remove element in list with help of pop
-
-# 4 value pop in list
-a.pop(4)
+b={'unty':40,'c.bro':15}
+a.update(b)
 print(a)
 
-# 44 value remove in list 
-a.pop(0)
+a={1,2,3,4}
+b={3,4,5,6}
+print(a|b)
+
+print(a.union(b))
+
+print(b.union(a))
+
+print(a&b)
+
+print(a.intersection(b))
+
+print(b.intersection(a))
+
+print(a-b)
+print(b-a)
+
+print(a.difference(b))
+c=set()
+c=a.copy()
+print(c)
+
+# c.copy(a)# tyError: set.copy() takes no arguments (1 given)
+# print(c)
+
+c=b.copy()
+print(c)
+
+a.add(7)
 print(a)
 
-# 23 value pop in list
-a.pop(1)
-print(a)
-
-# 5 value pop in list
-a.pop(2)
-print(a)
-
-# 4 value pop in list
-a.pop(3)
-print(a)
-
-#----------------------------------------------------------------------------------------------------------
-
-# add list in the list with help of extend
-
-
-b=[9,8,7,6]
-a.extend(b)
-print(a)
-
-c=["manu","nanu"]
-a.extend(c)
-print(a)
-
-d=["shamu",12,90]
-a.extend(d)
-print(a)
-
-e=[(1,2,3)]
-a.extend(e)
-print(a)
-
-f=[{56,78}]
-a.extend(f)
-print(a)
-
-#----------------------------------------------------------------------------------------------------------------
-
-# find value of with help find method
-# find index manu
-z=a.index("manu")
-print(z)
-
-# find index shamu
-z=a.index("shamu")
-print(z)
-
-
-# find index raj
-z=a.index("raj")
-print(z)
-
-
-# find index 90
-z=a.index(90)
-print(z)
-
-
-# find index 12
-z=a.index(12)
-print(z)
-
-# find index roshani
-
-#z=a.index("roshni")
-#print(z)
-
-# ValueError: 'roshni' is not in list
-#----------------------------------------------------------------------------------------------------------
-
-# cpoy for new variable in list
-z=a.copy()
-print(z)
-
-#----------------------------------------------------------------------------------------------------------
-
-#  reverse for list 
-a.reverse()
-print(a)
-
-#-----------------------------------------------------------------------------------------------------
-
-# count for no of variable existed in list
-a=a.count(6)
-print(a)
-
-#-----------------------------------------------------------------------------------------------------
-
-# sorted are list
-
-p=[10,38,5,6,74,2,8,6]
-p.sort()
-print(p)
-
-#----------------------------------------------------------------------------------------------
-# reverse sorted are list
-p.sort(reverse=True)
-print(p)
-
-#--------------------------------------------------------------------------------------------------
-# clear for list 
-z.clear()
-print(z)
-
-#------------------------------------------------------------------------------------------------
+# a.add(b)#tpeError: unhashable type: 'set'
+# print(a)
