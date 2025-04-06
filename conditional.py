3398611003399ef8c633868b5f5fcc2d2ade9c5a
Roshani salekar <roshanisalekar3046@gmail.com>
2025-04-06 07:29:52 -0700

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,91 @@
+# # write a programto check whther given age >18 or not 
+
+# a=int(input('enter a age :-'))
+# if a>18:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# # write a programto check whther given password correct or not
+
+# pw=str(input("enter a password :-"))
+# if pw=="raj":
+#     print("welcome to application")
+# else:
+#     print("password is incorrect")
+# print("thank you so much")
+
+# example :3
+
+# tea=10
+# coff=20
+# colddrink=30
+
+# product=str(input("enter a product :-"))
+
+# if product=="tea":
+#     print(tea)
+
+# elif product=="coff":
+#     print(coff)
+# elif product=="colddrink":
+#     print(colddrink)
+# else:
+#     print("product not found")
+
+# calculate salary  :4 
+
+# phy=int(input("enter a  phy marks :- "))
+# chem=int(input("enter a  phy marks :- "))
+# bio=int(input("enter a  phy marks :- "))
+# math=int(input("enter a  phy marks :- "))
+# cs=int(input("enter a  phy marks :- "))
+
+# mark=phy+chem+bio+math+cs
+
+# per=mark/100
+
+# if per>90 or per ==90:
+#     print("grade A")
+
+# elif per>80 or per ==80:
+#     print("grade B")
+
+# elif per>70 or per ==70:
+#     print("grade C")
+
+# elif per>60 or per ==60:
+#     print("grade D")
+
+# elif per>40 or per ==40:
+#     print("grade E")
+
+# elif per<40:
+#     print("grade f")
+
+# else:
+#     print('you are fail')
+
+
+# example : 5
+
+# mark=int(input("enter a marks :- "))
+# if mark>=80:
+#     print("A+")
+# elif mark>=70:
+#     print("A")
+# elif mark>=60:
+#     print("B+")
+# elif mark>=50:
+#     print("A")
+# elif mark>=40:
+#     print("A")
+# elif mark>=35:
+#     print("A")
+# else:
+#     print("FAIL")
+
+
+
+
