921e6a670404c46a82d1365916cccf49e5a0bee6
Roshani salekar <roshanisalekar3046@gmail.com>
2025-04-06 07:27:38 -0700

    Add files via upload

---
+++ conditional-ladder.py
@@ -0,0 +1,31 @@
+
+# phy=int(input("enter a  phy marks :- "))
+# chem=int(input("enter a  phy marks :- "))
+# bio=int(input("enter a  phy marks :- "))
+# math=int(input("enter a  phy marks :- "))
+# cs=int(input("enter a  phy marks :- "))
+
+# mark=phy+chem+bio+math+cs
+
+# per=mark/100
+
+# if per>90 or per ==90:
+#     print("grade A")
+
+# elif per>80 or per ==80:
+#     print("grade B")
+
+# elif per>70 or per ==70:
+#     print("grade C")
+
+# elif per>60 or per ==60:
+#     print("grade D")
+
+# elif per>40 or per ==40:
+#     print("grade E")
+
+# elif per<40:
+#     print("grade f")
+
+# else:
+#     print('you are fail')
---
+++ conditional-nested.py
@@ -0,0 +1,29 @@
+a=int(input("enter a value :-"))
+if a>1:
+    if a>=35:
+        if a>=40:
+            if a>50:
+                if a>=60:
+                    if a>=70:
+                        if a>=80:
+                            print("A+")
+                        else:
+                            print("A")
+                    else:
+                        print("B+")
+                else:
+                    print("B")
+            else:
+                print("c")
+        else:
+            print("D")
+    else:
+        print("fail")
+else:
+    print("value not found")
+
+
+# output :- 
+
+# enter a value :-56
+# B
